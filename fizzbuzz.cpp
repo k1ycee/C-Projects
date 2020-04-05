bc7adf0607b52a3c0675bf57bb47f24d852884ce
Denzel <chiagozieani145@gmail.com>
2020-04-05 12:47:20 +0100

    fizzbuzz

---
+++ fizzbuzz.cpp
@@ -0,0 +1,17 @@
+int main(){
+  for(int i=0; i<=100 ; i++){
+    // std::cout << i << "\n";
+    if(i%3 == 0 && i%5 ==0){
+     std::cout << "Fizz\n"; 
+    }
+    else if (i%5 == 0){
+      std::cout << "Buzz\n";
+    }
+    else if (i%3 == 0){
+      std::cout << "FizzBuzz\n";
+    }
+    else{
+      std::cout << i << "\n";
+    }
+  }
+}
