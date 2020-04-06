0bc72ed4a68ea17a2d1b666e92c70212c4dccb8d
Denzel <chiagozieani145@gmail.com>
2020-04-06 16:14:30 +0100

    It is a game but can be perfected

--- bottles.cpp
+++ bottles.cpp
@@ -1,8 +1,10 @@
+#include <iostream>
+
 int main() {
 
   // Write a for loop here:
-  for(int i=5; i>0; i--){
-     std::cout << i << " bottles of pop on the wall.\n";
+  for(int i=99; i>0; i--){
+    std::cout << i << " bottles of pop on the wall.\n";
     std::cout << "Take one down and pass it around.\n";
     std::cout << i - 1 << " bottles of pop on the wall.\n\n";
   }
